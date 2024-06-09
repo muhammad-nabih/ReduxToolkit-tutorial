06c0e9f49d1e59604d074e01c37777444e34216d
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-06-09 23:50:38 +0300

    from master branch

--- src/ReduxToolkit/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CounterSlice from "../Feature/Counter/CounterSlice";
-
-export const store = configureStore({
-  reducer: {
-    counter: CounterSlice,
-  },
-});
