3a29aa0afeae9d3f195f5e4b15186478e085566b
muhmmad Nabh <muhammadnabih2020@gmail.com>
2024-06-16 04:47:34 +0300

    initial commit

--- src/app/features/userSlice.jsx
+++ src/app/features/userSlice.jsx
@@ -5,7 +5,6 @@ export const userFetch = createAsyncThunk("users/fetchUsers", async () => {
   const response = await axios.get(
     "https://jsonplaceholder.typicode.com/users"
   );
-  console.log(response.data);
   return response.data;
 });
 
